c49da4132f2af3c26978dedc51694f319fc3528a
Alyssa Farina <alyssafarina695@gmail.com>
2019-07-17 17:56:02 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,8 @@
 class Artist
-
-   
-    @all = []
-
+    attr_accessor :name
+  
+    @@all = []
+  
     def initialize(name)
       @name = name
       @@all << self
@@ -29,3 +29,4 @@ class Artist
       Song.all.count
     end
   end
+  
